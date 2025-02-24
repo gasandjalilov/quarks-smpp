204049ba38b7b76ae3eafd585482e39dbb4d0029
djalilov.g <g.djalilov@corp.click.uz>
2025-02-24 11:35:37 +0500

    [removed] unused scopes

--- src/main/java/uz/click/tool/MyMessagingApplication.java
+++
@@ -1,9 +0,0 @@
-package uz.click.tool;
-
-import jakarta.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class MyMessagingApplication {
-
-
-}
--- src/test/java/uz/click/tool/MyMessagingApplicationTest.java
+++ src/test/java/uz/click/tool/MyMessagingApplicationTest.java
@@ -6,8 +6,6 @@ import jakarta.inject.Inject;
 @QuarkusTest
 class MyMessagingApplicationTest {
 
-    @Inject
-    MyMessagingApplication application;
 
 
 }
